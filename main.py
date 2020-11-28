b0892f564554bd3cabfe2f05848ecb45e1c33b20
lihesalo <64031196+lihesalo@users.noreply.github.com>
2020-11-28 13:50:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -89,21 +89,65 @@ Assignment 4
 
 """
 
+
+
 """
 
-interarrival_time = exp(25)
-prep_time = exp(40)
-rec_time = exp(40)
+interarrival_time is either exp(25) or exp(22.5)
+
+prep_time is either exp(40) or unif(30,50)
+rec_time is either exp(40) or unif(30,50)
+number of preparation rooms is either 4 or 5
+number of recovery rooms is either 4 or 5
 
 """
 
 print("\r\n\r\n")
 for k in range(61,80):
+    
     simulation_length = 1000
-    interarrival_time = randomize.exponential(25)
-    prep_time = randomize.exponential(40)
-    rec_time = randomize.exponential(40)
-    results = simulation.run_simulation(4,5, interarrival_time, prep_time, rec_time, simulation_length, k, False)
+    
+    interarrival_time = random.choice([randomize.exponential(25), randomize.exponential(22.5)])
+    prep_time = random.choice([randomize.exponential(40), randomize.unif(30,50)])
+    rec_time = random.choice([randomize.exponential(40), randomize.unif(30,50)])
+    results = simulation.run_simulation(random.choice([4,5]), random.choice([4,5])
+                    , interarrival_time, prep_time, rec_time, simulation_length, k, False)
+    
+    avg = stats.mean(results[0])
+    conf = stats.confidence95(results[0])
+    lower_bound = conf[0]
+    upper_bound = conf[1]
+    print("The average preparation queue length is %.2f with a 95%% confidence interval of [%.2f , %.2f]." % (avg, lower_bound, upper_bound))
+    operation_idle_time = results[1]
+    operation_idle_percentage = operation_idle_time / simulation_length * 100
+    print("Operation room was idle for %.0f time units, meaning the total probability of it being blocked at random time unit is %.2f %%" % (operation_idle_time, operation_idle_percentage))
+    
+    recovery_blocked = results[2] / results[3] * 100
+    print("The probability of recovery being full and operation being blocked is: ", recovery_blocked)
+    
+
+"""
+
+interarrival_time is either unif(25) or unif(22.5)
+
+prep_time is either exp(40) or unif(30,50)
+rec_time is either exp(40) or unif(30,50)
+number of preparation rooms is either 4 or 5
+number of recovery rooms is either 4 or 5
+
+"""
+
+print("\r\n\r\n")
+for k in range(61,80):
+    
+    simulation_length = 1000
+    
+    interarrival_time = random.choice([randomize.unif(20,30), randomize.unif(20,25)])
+    prep_time = random.choice([randomize.exponential(40), randomize.unif(30,50)])
+    rec_time = random.choice([randomize.exponential(40), randomize.unif(30,50)])
+    results = simulation.run_simulation(random.choice([4,5]), random.choice([4,5])
+                    , interarrival_time, prep_time, rec_time, simulation_length, k, False)
+    
     avg = stats.mean(results[0])
     conf = stats.confidence95(results[0])
     lower_bound = conf[0]
